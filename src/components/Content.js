43a95acae3c9737e632fc80e43973e51db8865d5
Eswarkartheekgrandhi <20pa1a0554@vishnu.edu.in>
2023-11-27 15:46:29 +0530

    changes done

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,36 +1,23 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState } from "react";
 import "./Content.css";
 
 function Content() {
-  const [isPopupVisible, setPopupVisible] = useState(false);
-  const [selectedImagesSet1, setSelectedImagesSet1] = useState([]);
-  const [selectedImagesSet2, setSelectedImagesSet2] = useState([]);
-  const [generatedImagesSet1, setGeneratedImagesSet1] = useState([]);
-  const [generatedImagesSet2, setGeneratedImagesSet2] = useState([]);
+  const [positivePrompt, setPositivePrompt] = useState("");
+  const [negativePrompt, setNegativePrompt] = useState("");
+  const [generatedImages, setGeneratedImages] = useState([]);
 
-  const togglePopup = () => {
-    setPopupVisible(!isPopupVisible);
-  };
-
-  const handleImageUploadSet1 = (e) => {
-    const files = e.target.files;
-    if (files.length > 0) {
-      setSelectedImagesSet1([...selectedImagesSet1, ...files]);
-    }
-  };
-
-  const handleImageUploadSet2 = (e) => {
-    const files = e.target.files;
-    if (files.length > 0) {
-      setSelectedImagesSet2([...selectedImagesSet2, ...files]);
-    }
-  };
-
-  const generateImages = () => {
-    setGeneratedImagesSet1(selectedImagesSet1);
-    setGeneratedImagesSet2(selectedImagesSet2);
-    setPopupVisible(false); // Close the popup
+  const handleGenerate = () => {
+    const staticImages = [
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+      "https://via.placeholder.com/150",
+    ];
+    setGeneratedImages(staticImages);
   };
 
   return (
@@ -41,99 +28,28 @@ function Content() {
           <textarea
             rows="1"
             cols="50"
-            placeholder="Enter your description"
+            placeholder="Enter Positive Prompt"
+            value={positivePrompt}
+            onChange={(e) => setPositivePrompt(e.target.value)}
           ></textarea>
-          <button className="generate-button" onClick={generateImages}>
-            Generate
-          </button>
+          <textarea
+            rows="1"
+            cols="50"
+            placeholder="Enter Negative Prompt"
+            value={negativePrompt}
+            onChange={(e) => setNegativePrompt(e.target.value)}
+          ></textarea>
         </div>
-        <p>
-          Or,{" "}
-          <span className="upload-text" onClick={togglePopup}>
-            upload
-          </span>{" "}
-          an image to edit
-        </p>
-        {isPopupVisible && <div className="overlay"></div>}
-        {isPopupVisible && (
-          <div className="popup">
-            <div className="popup-content">
-              <h5>Upload images here</h5>
-              <div className="popup-boxes">
-                <div className="popup-box">
-                  <p>Set 1</p>
-                  <label
-                    htmlFor="imageUploadSet1"
-                    className="image-upload-label"
-                  >
-                    <img src="upload.png" alt="upload" className="upload" />
-                  </label>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    id="imageUploadSet1"
-                    className="image-upload-input"
-                    onChange={handleImageUploadSet1}
-                    multiple // Enable multiple file selection
-                  />
-                </div>
-                <div className="popup-box">
-                  <p>Set 2</p>
-                  <label
-                    htmlFor="imageUploadSet2"
-                    className="image-upload-label"
-                  >
-                    <img src="upload.png" alt="upload" className="upload" />
-                  </label>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    id="imageUploadSet2"
-                    className="image-upload-input"
-                    onChange={handleImageUploadSet2}
-                    multiple // Enable multiple file selection
-                  />
-                </div>
-              </div>
-              <div className="popup-buttons">
-                <button className="popup-generate" onClick={generateImages}>
-                  Generate
-                </button>
-                <button className="popup-generate" onClick={togglePopup}>
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
+        <button className="generate-button" onClick={handleGenerate}>
+          Generate
+        </button>
       </div>
       <div className="generated-images">
         <h2>Generated Images</h2>
-        <div className="image-set">
-          <h3>Set 1 Images</h3>
-          <div className="image-grid">
-            {generatedImagesSet1.map((image, index) => (
-              <img
-                key={index}
-                src={URL.createObjectURL(image)}
-                alt={`Set 1 Image ${index + 1}`}
-                className="generated-image"
-              />
-            ))}
-          </div>
-        </div>
-        <div className="image-set">
-          <h3>Set 2 Images</h3>
-          <div className="image-grid">
-            {generatedImagesSet2.map((image, index) => (
-              <img
-                key={index}
-                src={URL.createObjectURL(image)}
-                alt={`Set 2 Image ${index + 1}`}
-                className="generated-image"
-              />
-            ))}
-          </div>
+        <div className="image-row">
+          {generatedImages.map((imageUrl, index) => (
+            <img key={index} src={"logo192.png"} alt={"generated images"} />
+          ))}
         </div>
       </div>
     </div>
